8aa472f5146d48e85f1e7a4516af8408bc59897b
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-02-22 19:55:16 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'application#home'
   get "/login",  to: "sessions#new"
   post "/", to: "sessions#create"
   delete '/logout', to: 'sessions#destroy'
