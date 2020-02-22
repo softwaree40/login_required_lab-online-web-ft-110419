e903616f7a4c09c517e1d419c6140260c2672bd4
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-02-22 19:53:40 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,17 @@
 class SessionsController < ApplicationController
-     def new
+    def new
   end
 
   def create
-    
     if !params[:name] || params[:name].empty?
       redirect_to login_path
     else
       session[:name] = params[:name]
       redirect_to root_path
     end
-    
   end
 
   def destroy
-    
     session.delete :name if session[:name]
     redirect_to root_path
   end
